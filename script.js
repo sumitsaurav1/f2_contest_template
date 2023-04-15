b0bd16bdb0fd1ef8f0d9634cb876d7a87b4777f2
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-04-15 18:18:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,16 +18,39 @@ function PrintDeveloperbyMap() {
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  function callBack(e){
+        if(e.profession=="developer"){
+            console.log(e);
+        }
+    }
+    arr.forEach(callBack)
 }
 
 function addData() {
   //Write your code here, just console.log
+    let obj= {id:4,name:"susan",age:"20",profession:"intern"};
+    arr.push(obj);
+    console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  for(let i=0;i<arr.length;i++){
+        if(arr[i].profession=="admin")
+        {
+           arr.splice(i,1);
+        }
+    }
+    console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+   let arr1 = [
+        { id: 4, name: "adam", age: "25", profession: "Designer" },
+        { id: 5, name: "sins", age: "30", profession: "Engineer" },
+        { id: 6, name: "kiran", age: "17", profession: "student" },
+      ];
+      let childArr=arr.concat(arr1);
+      console.log(childArr);
 }
