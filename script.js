04f3f6ed8d1d8fa846ea7ee64d1f14977432cf6f
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-04-15 18:15:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ let arr = [
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
   function callBack(e){
-    if(e.profession=="developer")
-    {
-      console.log(e)
+      if(e.profession=="developer"){
+        console.log(e)
+      }
     }
-  }
+    arr.map(callBack);
 }
 
 function PrintDeveloperbyForEach() {
